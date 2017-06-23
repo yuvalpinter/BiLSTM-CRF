a6ca559210e2de42cca7a7f995ababd3561f2150
Yuval Pinter <uvp@gatech.edu>
2017-06-23 17:37:22 -0400

    added mimick model saving and loading

--- mimick/model.py
+++ mimick/model.py
@@ -22,28 +22,38 @@ DEFAULT_HIDDEN_DIM = 50
 
 Instance = collections.namedtuple("Instance", ["chars", "word_emb"])
 
-class LSTMPredictor:
+class LSTMMimick:
 
-    def __init__(self, num_lstm_layers, charset_size, char_dim, hidden_dim, word_embedding_dim, vocab_size=None):
+    def __init__(self, num_lstm_layers=-1, charset_size=-1,\
+                char_dim=-1, hidden_dim=-1, word_embedding_dim=-1, file=None):
         self.model = dy.Model()
-        
-        # Char LSTM Parameters
-        self.char_lookup = self.model.add_lookup_parameters((charset_size, char_dim))
-        self.char_fwd_lstm = dy.LSTMBuilder(num_lstm_layers, char_dim, hidden_dim, self.model)
-        self.char_bwd_lstm = dy.LSTMBuilder(num_lstm_layers, char_dim, hidden_dim, self.model)
-        
-        # Post-LSTM Parameters
-        self.lstm_to_rep_params = self.model.add_parameters((word_embedding_dim, hidden_dim * 2))
-        self.lstm_to_rep_bias = self.model.add_parameters(word_embedding_dim)
-        self.mlp_out = self.model.add_parameters((word_embedding_dim, word_embedding_dim))
-        self.mlp_out_bias = self.model.add_parameters(word_embedding_dim)
+        if file == None:
+            # Char LSTM Parameters
+            self.char_lookup = self.model.add_lookup_parameters((charset_size, char_dim))
+            self.char_fwd_lstm = dy.LSTMBuilder(num_lstm_layers, char_dim, hidden_dim, self.model)
+            self.char_bwd_lstm = dy.LSTMBuilder(num_lstm_layers, char_dim, hidden_dim, self.model)
+
+            # Post-LSTM Parameters
+            self.lstm_to_rep_params = self.model.add_parameters((word_embedding_dim, hidden_dim * 2))
+            self.lstm_to_rep_bias = self.model.add_parameters(word_embedding_dim)
+            self.mlp_out = self.model.add_parameters((word_embedding_dim, word_embedding_dim))
+            self.mlp_out_bias = self.model.add_parameters(word_embedding_dim)
+        else:
+            model_members = iter(self.model.load(file))
+            self.char_lookup = model_members.next()
+            self.char_fwd_lstm = model_members.next()
+            self.char_bwd_lstm = model_members.next()
+            self.lstm_to_rep_params = model_members.next()
+            self.lstm_to_rep_bias = model_members.next()
+            self.mlp_out = model_members.next()
+            self.mlp_out_bias = model_members.next()
 
     def predict_emb(self, chars):
         dy.renew_cg()
-        
+
         finit = self.char_fwd_lstm.initial_state()
         binit = self.char_bwd_lstm.initial_state()
-        
+
         H = dy.parameter(self.lstm_to_rep_params)
         Hb = dy.parameter(self.lstm_to_rep_bias)
         O = dy.parameter(self.mlp_out)
@@ -55,7 +65,7 @@ class LSTMPredictor:
 
         bi_fwd_out = finit.transduce(embeddings)
         bi_bwd_out = binit.transduce(reversed(embeddings))
-        
+
         rep = dy.concatenate([bi_fwd_out[-1], bi_bwd_out[-1]])
 
         return O * dy.tanh(H * rep + Hb) + Ob
@@ -63,7 +73,7 @@ class LSTMPredictor:
 
     def loss(self, observation, target_rep):
         return dy.squared_distance(observation, dy.inputVector(target_rep))
-    
+
     def set_dropout(self, p):
         self.char_fwd_lstm.set_dropout(p)
         self.char_bwd_lstm.set_dropout(p)
@@ -75,7 +85,8 @@ class LSTMPredictor:
     def save(self, file_name):
         members_to_save = []
         members_to_save.append(self.char_lookup)
-        members_to_save.append(self.char_bi_lstm)
+        members_to_save.append(self.char_fwd_lstm)
+        members_to_save.append(self.char_bwd_lstm)
         members_to_save.append(self.lstm_to_rep_params)
         members_to_save.append(self.lstm_to_rep_bias)
         members_to_save.append(self.mlp_out)
@@ -85,17 +96,17 @@ class LSTMPredictor:
     @property
     def model(self):
         return self.model
- 
+
 
 def wordify(instance):
     return ''.join([i2c[i] for i in instance.chars])
-    
+
 def dist(instance, vec):
     we = instance.word_emb
     if options.cosine:
         return 1.0 - (we.dot(vec) / (np.linalg.norm(we) * np.linalg.norm(vec)))
     return np.linalg.norm(we - vec)
- 
+
 # ===-----------------------------------------------------------------------===
 # Argument parsing
 # ===-----------------------------------------------------------------------===
@@ -103,6 +114,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--dataset", required=True, dest="dataset", help=".pkl file to use")
 parser.add_argument("--vocab", required=True, dest="vocab", help="total vocab to output")
 parser.add_argument("--output", required=True, dest="output", help="file with all embeddings")
+parser.add_argument("--model-out", dest="model_out", help="file with model parameters (optional)")
 parser.add_argument("--lang", dest="lang", default="en", help="language")
 parser.add_argument("--char-dim", default=DEFAULT_CHAR_DIM, dest="char_dim", help="dimension for character embeddings (default = 20)")
 parser.add_argument("--hidden-dim", default=DEFAULT_HIDDEN_DIM, dest="hidden_dim", help="dimension for LSTM layers (default = 50)")
@@ -146,7 +158,7 @@ with codecs.open(options.vocab, "r", "utf-8") as vocab_file:
     for vw in vocab_file.readlines():
         vocab_words[vw.strip()] = np.array([0])
 
-model = LSTMPredictor(options.num_lstm_layers, len(c2i), options.char_dim, options.hidden_dim, emb_dim, vocab_size=None)
+model = LSTMMimick(options.num_lstm_layers, len(c2i), options.char_dim, options.hidden_dim, emb_dim)
 trainer = dy.MomentumSGDTrainer(model.model, options.learning_rate, 0.9, 0.1)
 root_logger.info("Training Algorithm: {}".format(type(trainer)))
 
@@ -174,30 +186,30 @@ inferred_vec_norms = 0.0
 # Shuffle set, divide into cross-folds each epoch
 for epoch in xrange(epcs):
     bar = progressbar.ProgressBar()
-    
+
     train_loss = 0.0
     train_correct = Counter()
     train_total = Counter()
 
     if options.dropout > 0:
         model.set_dropout(options.dropout)
-    
+
     for instance in bar(train_instances):
         if len(instance.chars) <= 0: continue
         obs_emb = model.predict_emb(instance.chars)
         loss_expr = model.loss(obs_emb, instance.word_emb)
         loss = loss_expr.scalar_value()
-        
+
         # Bail if loss is NaN
         if math.isnan(loss):
             assert False, "NaN occured"
-            
+
         train_loss += loss
 
         # Do backward pass and update parameters
         loss_expr.backward()
         trainer.update()
-        
+
         if epoch == epcs - 1:
             word = wordify(instance)
             if word in vocab_words:
@@ -207,24 +219,24 @@ for epoch in xrange(epcs):
                     inferred_vec_norms += np.linalg.norm(vocab_words[word])
                 else: # log vocab embeddings
                     vocab_words[word] = instance.word_emb
-        
+
     root_logger.info("\n")
     root_logger.info("Epoch {} complete".format(epoch + 1))
     trainer.update_epoch(1)
     print trainer.status()
-    
+
     # Evaluate dev data
     model.disable_dropout()
     dev_loss = 0.0
     dev_correct = Counter()
     dev_total = Counter()
-    
+
     bar = progressbar.ProgressBar()
     for instance in bar(dev_instances):
         if len(instance.chars) <= 0: continue
         obs_emb = model.predict_emb(instance.chars)
         dev_loss += model.loss(obs_emb, instance.word_emb).scalar_value()
-        
+
         if epoch == epcs - 1:
             word = wordify(instance)
             if word in vocab_words:
@@ -234,7 +246,7 @@ for epoch in xrange(epcs):
                     inferred_vec_norms += np.linalg.norm(vocab_words[word])
                 else: # log vocab embeddings
                     vocab_words[word] = instance.word_emb
-    
+
     root_logger.info("Train Loss: {}".format(train_loss))
     root_logger.info("Dev Loss: {}".format(dev_loss))
 
@@ -250,7 +262,7 @@ for idx, instance in enumerate(test_instances):
     obs_emb = model.predict_emb(instance.chars)
     vocab_words[word] = np.array(obs_emb.value())
     inferred_vec_norms += np.linalg.norm(vocab_words[word])
-    
+
     # reservoir sampling
     if idx < showcase_size:
         showcase.append(word)
@@ -258,7 +270,7 @@ for idx, instance in enumerate(test_instances):
         rand = random.randint(0,idx-1)
         if rand < showcase_size:
             showcase[rand] = word
-    
+
 root_logger.info("Average norm for trained: {}".format(inferred_vec_norms / len(test_instances)))
 
 similar_words = {}
@@ -269,7 +281,7 @@ for w in showcase:
         print w, [(i,d) for i,d in top_k]
     similar_words[w] = top_k
 
-    
+
 # write all
 with codecs.open(options.output, "w", "utf-8") as writer:
     for vw, emb in vocab_words.iteritems():
@@ -278,6 +290,9 @@ with codecs.open(options.output, "w", "utf-8") as writer:
             writer.write(str(i) + " ")
         writer.write("\n")
 
-# TODO save model
+# save model
+if options.model_out is not None:
+    model.save(options.model_out)
 
+### demo commented out for pesky encoding issues ###
 #root_logger.info("\nSome most-similar words from training set for a random selection of test set:\n{}".format("\n".join([k + ":\t" + " ".join([t[0] for t in v]) for k,v in similar_words.iteritems()])))
